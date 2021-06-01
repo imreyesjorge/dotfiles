672fb5dbc0af01c743d24cafad8b393548f2059e
Jorge Reyes <reyestrevjorge@gmail.com>
2021-05-31 20:26:48 -0500

    :recycle: update: `sxhkdrc` and `config.fish`

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -4,3 +4,7 @@ set fish_greeting ""
 # custom aliases
 alias ll="exa -l"
 alias la="exa -al"
+alias neofetch="neofetch --ascii_distro archlabs"
+alias piconnect="ssh pi@192.168.1.66"
+alias py="python3"
+alias prettyjson="python -m json.tool"
