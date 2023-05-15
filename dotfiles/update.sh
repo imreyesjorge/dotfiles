d1f9c0568f61ef7aeb1c37885ba107de2dd51b95
Jorge Reyes <0706868+imreyesjorge@users.noreply.github.com>
2023-05-14 21:16:18 -0600

    Update dotfiles

--- dotfiles/update.sh
+++ dotfiles/update.sh
@@ -2,9 +2,3 @@
 
 # Update Leftwm
 rm -rf leftwm && cp -r $HOME/.config/leftwm .
-
-# Update Eww
-rm -rf eww && cp -r $HOME/.config/eww .
-
-# Update Dunst
-rm -rf dunst && cp -r $HOME/.config/dunst .
